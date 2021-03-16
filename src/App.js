db1ed53e9394090945da0ad83705bf0ead69a96b
Camilo Posada <camiloposada@Camilos-MacBook-Pro.local>
2021-03-15 20:54:29 -0500

    Signup component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './pages/shop/shoppage.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,17 +20,29 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state);
+        });
+        
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
-
-
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
 
   render() {
     return( 
@@ -45,6 +57,5 @@ class App extends React.Component {
     );
   }
 }
-
 
 export default App;
