5c735973e83febc55ceef1169a060ba67ab672a6
Camilo <cposadaa@gmail.com>
2021-04-10 12:24:38 -0500

    redirect to home if user is signed in, create user actipn types enum

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import HomePage from './pages/homepage/homepage.component';
@@ -47,7 +47,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+            exact 
+            path='/signin' 
+            render= { () => 
+              this.props.currentUser ? 
+              (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage/>
+              )
+            } 
+          />
         </Switch>
       </div>
     );
@@ -58,5 +69,7 @@ const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-// the first argument is null because we don't need state to props'
-export default connect(null, mapDispatchToProps)(App);
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+export default connect(mapStateToProps, mapDispatchToProps)(App);
