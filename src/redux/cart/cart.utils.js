d99960c314d57813abd92cc4b46dcdf210210eaf
Camilo <cposadaa@gmail.com>
2021-04-17 12:55:49 -0500

    Add item and remove item to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -14,3 +14,12 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
+
+export const removeItemFromCart = (cartItems, itemToRemove) =>
+  cartItems.flatMap((item) => {
+    return item.id !== itemToRemove.id
+      ? item
+      : item.quantity === 1
+      ? []
+      : { ...item, quantity: item.quantity - 1 };
+  });
